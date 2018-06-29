29f092ad65ebb7cbc41c513dec1e51bc0ed32a32
larry glebov <larry.glebov@gmail.com>
2018-06-29 08:51:19 +0300

    front-page in progress"

--- js/all.js
+++ js/all.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    // for burger-menu
+    $('.x-burger-menu').on('click', function() {
+        $(this).toggleClass('header-bottom__burger-menu--active');
+        $('.x-menu', $(this)).toggleClass('header-bottom__menu--active');
+    });
+
+    // for instagram
     var token = '281996503.6885368.8a3f2b0d96be4e82a6807bf3eb5aaccd', // я уже давал ссылку чуть выше
         userId = 281996503, // ID пользователя, можно выкопать в исходном HTML, можно использовать спец. сервисы либо просто смотрите следующий пример :)
         count = 4; // ну это понятно - сколько фоток хотим вывести
@@ -9,7 +16,6 @@ $(document).ready(function () {
         type: 'GET',
         data: {access_token: token, count: count}, // передаем параметры, которые мы указывали выше
         success: function(result){
-            console.log(result);
             for( x in result.data ){
                 $('.x-instagram-list').append('<li class="instagram-list__item x-instagram-item" style="background-image: url('+result.data[x].images.low_resolution.url+')" data-url="'+result.data[x].link+'"></li>'); // result.data[x].images.low_resolution.url - это URL картинки среднего разрешения, 306х306
                 // result.data[x].images.thumbnail.url - URL картинки 150х150
