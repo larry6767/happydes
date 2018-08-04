1125bccf728b3d0b03a7b1fb189c43247aff4506
larry glebov <larry.glebov@gmail.com>
2018-08-04 12:26:45 +0300

    added lightbox

--- functions.php
+++ functions.php
@@ -9,15 +9,15 @@ remove_action('wp_head', 'wp_generator');
 
 // advanced custom fields header-footer edit
 if( function_exists('acf_add_options_page') ) {
-	
+
 	acf_add_options_page();
-	acf_add_options_sub_page('Header-Footer');	
-} 
+	acf_add_options_sub_page('Header-Footer');
+}
 
 
 
 function happ_setup() {
-		
+
 		//автоустановка тега title на страницы
 		add_theme_support( 'title-tag' );
 
@@ -51,13 +51,13 @@ function happ_scripts() {
 
 	wp_enqueue_style('all.min.css', THEME_DIR . '/css/all.min.css');
 
-	wp_enqueue_script('jquery.fn.uiModal', THEME_DIR . '/js/jquery.fn.uiModal.js', array("jquery"), '', true);
+	wp_enqueue_script('lightbox.min', THEME_DIR . '/js/lightbox.min.js', array("jquery"), '', true);
 
 	wp_enqueue_script('slick.min', THEME_DIR . '/js/slick.min.js', array("jquery"), '', true);
 
 	wp_enqueue_script('jquery.maskedinput.min', THEME_DIR . '/js/jquery.maskedinput.min.js', array("jquery"), '', true);
 
-	wp_enqueue_script('all.script', THEME_DIR . '/js/all.js', array("jquery"), '', true); 
+	wp_enqueue_script('all.script', THEME_DIR . '/js/all.js', array("jquery"), '', true);
 //___________________________
 
 	wp_enqueue_script( 'happ-navigation', THEME_DIR . '/js/navigation.js', array(), '20151215', true );
@@ -75,15 +75,15 @@ add_action( 'wp_enqueue_scripts', 'happ_scripts' );
 
 
 function remove_admin_menu() {
-	// remove_menu_page('options-general.php'); // Удаляем раздел Настройки	
+	// remove_menu_page('options-general.php'); // Удаляем раздел Настройки
   	remove_menu_page('tools.php'); // Инструменты
 	// remove_menu_page('users.php'); // Пользователи
 	// remove_menu_page('plugins.php'); // Плагины
-	// remove_menu_page('themes.php'); // Внешний вид	
+	// remove_menu_page('themes.php'); // Внешний вид
 	remove_menu_page('edit.php'); // Посты блога
 	// remove_menu_page('upload.php'); // Медиабиблиотека
 	// remove_menu_page('edit.php?post_type=page'); // Страницы
-	remove_menu_page('edit-comments.php'); // Комментарии	
+	remove_menu_page('edit-comments.php'); // Комментарии
 	// remove_menu_page('link-manager.php'); // Ссылки
   	// remove_menu_page('wpcf7');   // Contact form 7
 	// remove_menu_page('options-framework'); // Cherry Framework
